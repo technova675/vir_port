a8baca65cc0f56ce91163aa2c51a0e3d2af6cd07
technova675 <gameops25@gmail.com>
2026-08-13 04:25:51 +0530

    add brand logo

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -2,7 +2,7 @@ export default function BrandMark() {
   return (
     <div className="brand-mark" aria-hidden="true">
       <span className="brand-mark-vir">VIR</span>
-      <span className="brand-mark-rule" />
+      <span className="brand-mark-rule">/</span>
       <span className="brand-mark-tag">Portfolio</span>
     </div>
   );
